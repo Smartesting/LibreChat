049ab1dab0f0347490c7ae356f966393a2359c5c
Arnaud Colin <arnaud.colin@smartesting.com>
2025-05-26 11:39:36 +0200

    feat(client): Remove preset menu

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -1,25 +1,18 @@
-import { useMemo } from 'react';
 import { useOutletContext } from 'react-router-dom';
-import { getConfigDefaults, PermissionTypes, Permissions } from 'librechat-data-provider';
+import { Permissions, PermissionTypes } from 'librechat-data-provider';
 import type { ContextType } from '~/common';
 import ModelSelector from './Menus/Endpoints/ModelSelector';
-import { PresetsMenu, HeaderNewChat } from './Menus';
+import { HeaderNewChat } from './Menus';
 import { useGetStartupConfig } from '~/data-provider';
 import ExportAndShareMenu from './ExportAndShareMenu';
-import { useMediaQuery, useHasAccess } from '~/hooks';
+import { useHasAccess, useMediaQuery } from '~/hooks';
 import BookmarkMenu from './Menus/BookmarkMenu';
 import { TemporaryChat } from './TemporaryChat';
 import AddMultiConvo from './AddMultiConvo';
 
-const defaultInterface = getConfigDefaults().interface;
-
 export default function Header() {
   const { data: startupConfig } = useGetStartupConfig();
   const { navVisible } = useOutletContext<ContextType>();
-  const interfaceConfig = useMemo(
-    () => startupConfig?.interface ?? defaultInterface,
-    [startupConfig],
-  );
 
   const hasAccessToBookmarks = useHasAccess({
     permissionType: PermissionTypes.BOOKMARKS,
@@ -39,9 +32,8 @@ export default function Header() {
         <div className="mx-2 flex items-center gap-2">
           {!navVisible && <HeaderNewChat />}
           {<ModelSelector startupConfig={startupConfig} />}
-          {interfaceConfig.presets === true && interfaceConfig.modelSelect && <PresetsMenu />}
-          {hasAccessToBookmarks === true && <BookmarkMenu />}
-          {hasAccessToMultiConvo === true && <AddMultiConvo />}
+          {hasAccessToBookmarks && <BookmarkMenu />}
+          {hasAccessToMultiConvo && <AddMultiConvo />}
           {isSmallScreen && (
             <>
               <ExportAndShareMenu
